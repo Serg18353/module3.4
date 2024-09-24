3ea373f32a3cfa1b41fd9eaceb0da785550626c8
Serg18353 <mikholenok@yandex.ru>
2024-09-24 19:18:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-def print_params(a=1, b='строка', c=True):
-    print(a, b, c)
-
-
-print_params(5, 4, "acdc")
-print_params(b=25)
-print_params()
-values_list = ['kiss', 23, False]
-values_dict = {'a': 1, 'b': 'строка', 'c': True}
-print_params(values_list)
-print_params(*values_list)
-print_params(**values_dict)
-values_list_2 = ['RHCP', 22.3]
-print_params(*values_list_2, 42)
+def single_root_words(root_woordd, *other_woords):
+    root_woord_l = root_woordd.lower()
+    same_words = []
+    for i in other_woords:
+        other_woords_l = i.lower()
+        if root_woord_l in other_woords_l or other_woords_l in root_woord_l:
+            same_words.append(i)
+    return same_words
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+print(result1)
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result2)
